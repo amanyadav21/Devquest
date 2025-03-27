e30277cb2d493c210603d199506a764ac2adad7d
amanyadav21 <amankumaryadav1029@gmail.com>
2025-03-27 21:16:54 +0530

    upgrade in code

--- CQB/vite.config.js
+++ CQB/vite.config.js
@@ -1,18 +1,13 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// });
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    tailwindcss(),
+    react()
+  ],
   server: {
     open: true, // Automatically open the app in the browser on server start
     port: 3000, // Specify the port number
